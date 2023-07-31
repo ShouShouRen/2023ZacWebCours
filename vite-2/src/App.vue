7208d86ac8b6a2b7ee5ca980af91e4cf7d9f0309
Peter Chen <peterchen2017125@gmail.com>
2023-07-31 21:40:47 +0800

    20230731

--- vite-2/src/App.vue
+++ vite-2/src/App.vue
@@ -1,20 +1,39 @@
+<template>
+  <div>
+    <h1>Vue</h1>
+    <Nav></Nav>
+    <Transition name="fade">
+      <div>
+        <router-view></router-view>
+      </div>
+    </Transition>
+    <hr>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
 <script>
 import Nav from './components/Nav.vue';
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   data() {
     return {}
   },
   components: {
-    Nav,HelloWorld
+    Nav, HelloWorld
   }
 }
 </script>
-<template>
-  <h1>Vue</h1>
-  <Nav></Nav>
-  <router-view></router-view>
-  <hr>
-  <HelloWorld />
-</template>
-<style></style>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s, transform 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(20px);
+}
+
+</style>
--- vite-2/src/components/HelloWorld.vue
+++ vite-2/src/components/HelloWorld.vue
@@ -3,19 +3,35 @@
     <a href="#" @click="currentPanel = 'P1'">panel 1</a>
     <a href="#" @click="currentPanel = 'P2'">panel 2</a>
     <a href="#" @click="currentPanel = 'P3'">panel 3</a>
-    <component :is="currentPanel"></component>
+    <Transition>
+      <component :is="currentPanel"></component>
+    </Transition>
   </div>
 </template>
 <script>
-  import P1 from '@/components/P1.vue'
-  import P2 from '@/components/P2.vue'
-  import P3 from '@/components/P3.vue'
-  export default {
-    data(){
-      return {
-        currentPanel: 'P1'
-      }
-    },
-    components:{P1,P2,P3}
-  }
+import P1 from '@/components/P1.vue'
+import P2 from '@/components/P2.vue'
+import P3 from '@/components/P3.vue'
+export default {
+  data() {
+    return {
+      currentPanel: 'P1'
+    }
+  },
+  components: { P1, P2, P3 }
+}
 </script>
+<style>
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  position: absolute;
+  transform: translateY(20px);
+  opacity: 0;
+}
+</style>
